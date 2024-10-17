9cd97a7a77ab672bcd0632a119a6ad5ff1a467e0
FarlanTuruu <Farlanking01@gmail.com>
2024-10-17 20:21:56 +0700

    modul2

---
+++ Tugas1/Script.js
@@ -0,0 +1,18 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  function calculate() {
+    try {
+      let display = document.getElementById("display");
+      display.value = eval(display.value);
+    } catch (error) {
+      alert("Invalid input!");
+      clearDisplay();
+    }
+  }
+  
